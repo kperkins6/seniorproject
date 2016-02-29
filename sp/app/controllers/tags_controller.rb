5f947fdf2de2523e32c974d883cc70038c1a9bff
Kevin Perkins <kperkins6@mail.csuchico.edu>
2016-02-29 14:29:26 -0800

    Fixed another naming problem

--- sp/app/controllers/tags_controller.rb
+++ sp/app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   def index
       @tags = if params[:keywords]
-               Recipe.where('name ilike ?',"%#{params[:keywords]}%")
+               Tag.where('name ilike ?',"%#{params[:keywords]}%")
              else
                []
              end
