3f8879da622ef7650cab736aa77f9ace06365a83
Kevin Perkins <kperkins6s@mail.csuchico.edu>
2016-02-29 16:33:27 -0500

    Added a new migration and business cards and business card helper

--- sp/config/routes.rb
+++ sp/config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'tags/index'
 
   root 'home#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- sp/db/migrate/20160229021815_create_decks.rb
+++ sp/db/migrate/20160229021815_create_decks.rb
@@ -1,7 +1,7 @@
 class CreateDecks < ActiveRecord::Migration
   def change
     create_table :decks do |t|
-      t.int :user_id
+      t.integer :user_id
       t.text :business_cards
       t.string :description
 
